6a1182e728ed0da9dda0eb7622ff13c610c19458
amarroquin4 <alfredomarroquin83@gmail.com>
2020-10-16 08:59:50 -0700

    edit href

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
   <!--Navigation-->
   <nav class="navbar navbar-expand-md navbar-light sticky-top">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/index.html"><img style="width: 60px;" src="images/logo.jpg"></a>
+      <a class="navbar-brand" href="/index.php"><img style="width: 60px;" src="images/logo.jpg"></a>
       <div class="navbar-title">
         <h2>Introduction To Hiking</h2>
       </div>
@@ -29,10 +29,10 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="index.html">Home</a>
+            <a class="nav-link" href="index.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="about.html">About</a>
+            <a class="nav-link" href="about.php">About</a>
           </li>
         </ul>
       </div>
